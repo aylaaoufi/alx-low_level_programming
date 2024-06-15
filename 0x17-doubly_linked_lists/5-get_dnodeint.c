63be2b42f794b4cb5e9116a90f5b4450f368658e
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-15 08:38:16 +0100

    return nth node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list.
+ * @head: pointer of the head of the list.
+ * @index: starting from 0.
+ * Return: nth index or NULL.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
