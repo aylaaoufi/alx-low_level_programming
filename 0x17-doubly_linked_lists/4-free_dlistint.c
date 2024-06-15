a86985ce9df9dec433664b91b78b85ce77929821
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-15 08:17:14 +0100

    free all node

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list.
+ * @head: pointer of the head of the list.
+ * Return: void.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t = *tmp;
+
+	while(head != NULL)
+	{
+		tmp = head->next;
+		head = tmp;
+		free(tmp);
+	}
+}
