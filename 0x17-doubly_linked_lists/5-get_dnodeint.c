c4f76b751b68e7badfe349a59c11a2ab446cec60
aylaaoufi <ayoub.laaoufi@hotmail.com>
2024-06-15 08:47:51 +0100

    c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,8 +15,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (i == index)
 			return (head);
-		head = head->next;
 		i++;
+		head = head->next;
 	}
 	return (NULL);
 }
